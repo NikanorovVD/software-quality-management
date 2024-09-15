9918d0de11932cee0766ea5e29343d3d1be3a5f9
NikanorovVD <nikanorovvsevolod1@gmail.com>
2024-09-15 15:13:31 +0300

    Initial 1-2

---
+++ UKPO_1/UKPO_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string input;
+	std::cin >> input;
+
+	int len = input.length();
+	for (int i = 0; i < len / 2; i++)
+	{
+		int j = len - i - 1;
+		char buff = input[i];
+
+		input[i] = input[j];
+		input[j] = buff;
+	}
+	std::cout << input << std::endl;
+}
+
+
---
+++ UKPO_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace UKPO_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (double x = -0.4; x <= 1.3; x += 0.1)
+            {
+                Console.WriteLine(string.Format("f({0}) == {1}", x, Func(x)));
+            }
+        }
+
+        static double Func(double x) 
+        {
+            if (x <= 0.5) return Math.Sin(Math.PI / 2);
+            return Math.Sin((x - 1) * Math.PI / 2);
+        }
+    }
+}
