8bf311d28ce3f8085b97a4ed62a22cc1c3cc839a
NikanorovVD <nikanorovvsevolod1@gmail.com>
2024-09-15 16:34:01 +0300

    lab-3

---
+++ UKPO_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UKPO_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите кол-во строк массива : ");
+            int rows_count = int.Parse(Console.ReadLine());
+            Console.Write("Введите кол-во столбцов массива : ");
+            int columns_count = int.Parse(Console.ReadLine());
+
+            Random random = new();
+            int[,] table = new int[rows_count, columns_count];
+            List<int> alternating_сolumns = new();
+
+            for (int i = 0; i < rows_count; i++)
+            {
+                for (int j = 0; j < columns_count; j++)
+                {
+                    table[i, j] = random.Next() % 2;
+                }
+            }
+
+            for (int i = 0; i < columns_count; i++)
+            {
+                bool two_equals_in_raw = false;
+                for (int j = 1; j < rows_count; j++)
+                {
+                    if (table[j, i] == table[j - 1, i])
+                    {
+                        two_equals_in_raw = true;
+                        break;
+                    }
+                }
+
+                if (!two_equals_in_raw)
+                    alternating_сolumns.Add(i);
+            }
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < rows_count; i++)
+            {
+                for (int j = 0; j < columns_count; j++)
+                {
+                    Console.Write(table[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Номера строго чередующихся столбцов(0-based): " +
+                $"{(alternating_сolumns.Any() ? string.Join(' ', alternating_сolumns) : "отсутствуют")}");
+        }
+    }
+}
